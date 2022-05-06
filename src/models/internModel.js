5910241cfaab032255864cda606318cda701a04a
suniljnvs <sunilkumar0551998@gmail.com>
2022-05-06 16:16:23 +0530

    error fixed

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -2,18 +2,34 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const internSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: {
+        type: String,
+        required: true
+    },
 
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
 
-    mobile: { type: Number, required: true, unique: true },
+    mobile: {
+        type: Number,
+        required: true,
+        unique: true
+    },
 
-    collegeId: { type: ObjectId, ref: "College", required: true },
+    collegeId: {
+        type: ObjectId,
+        ref: "College",
+        required: true
+    },
 
-    isDeleted: { type: Boolean, default: false },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
 
-}, {
-    timestamps: true,
-});
+}, { timestamps: true, });
 
 module.exports = mongoose.model("Intern", internSchema);
